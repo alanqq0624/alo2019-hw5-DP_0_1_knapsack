312b0a2ec4f894deb88ea5358c78936c79d8087d
alanqq0624 <alanqq0624@hotmail.com>
2019-05-13 22:40:07 +0800

    Formate main program

--- DP_0_1_knapsack.cpp
+++ DP_0_1_knapsack.cpp
@@ -10,7 +10,8 @@ vector<int> itemValue, itemWeight;
 
 void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W);
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
     cout << "Dynamic Programming 0-1 Knapsack Problem\n\n" ;
 
     // init variable------------------------------------------------------------------------------
@@ -25,14 +26,14 @@ int main(int argc, char const *argv[]){
     }
 
     // get W or aimWeight---------------------------------------------------------
-    if(argc != 2){
+    if(argc != 2) {
         cout << "Usage: ./DP_0_1_knapsack W\n";
         return 1;
     }
     int k_in = atoi(argv[1]);
     aimWeiget = k_in;
     cout << "aimWeight: " << aimWeiget << "\n";
-    
+
     // set item 0 to zero
     itemValue.push_back(0);
     itemWeight.push_back(0);
@@ -70,20 +71,21 @@ int main(int argc, char const *argv[]){
     DP_0_1_knapsack(itemValue, itemWeight, itemNum, aimWeiget);
 
     // print answer ------------------------------------------------------------------------------------
-    
+
     cout << "END\n\n";
     return 0;
 }
 
-/*  
-    input parameter": 
+/*
+    input parameter":
         v: input item value list
         w: input item weight list
         n: input item's number
         W: weight restriction
 */
-void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W){
-    
+void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W)
+{
+
     // assign 2D array to save DP optimal value and init all c to 0------------------------------------------------------
     // c[item that current checking][ristric weight]
 
@@ -93,12 +95,12 @@ void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W){
     // for(int i = 0; i < W; i++){
     //     c[i] = new int[W];
     // }
-    
+
     // way 2: use vector
     vector<vector<int>> c(n+1);
-    for (int i = 0; i < n+1; i++) { 
-        c[i] = vector<int>(W+1); 
-        for (int j = 0; j < W+1; j++){
+    for (int i = 0; i < n+1; i++) {
+        c[i] = vector<int>(W+1);
+        for (int j = 0; j < W+1; j++) {
             c[i][j] = 0;
             // cout << "c[" << i << "][" << j << "]: " << c[i][j] << "\n";
         }
@@ -110,36 +112,34 @@ void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W){
     for (int i = 0; i < n+1; i++) {
         p[i] = vector<vector<int>>(W+1);
     }
-    
+
     // compute ooptimal value ---------------------------------------------------------------------------------------------------
     // cout << "i'th\tw ris\ti'w\tway\tpattern\tfin val\n";
-    for (int i = 1 ; i <= n; i++){
-        for (int j = 1; j <= W; j++){
+    for (int i = 1 ; i <= n; i++) {
+        for (int j = 1; j <= W; j++) {
             // cout << i << "\t" << j << "\t" << w[i] << "\t";
-            if (w[i] <= j){
-                if (v[i] + c[i-1][j-w[i]] > c[i-1][j]){
+            if (w[i] <= j) {
+                if (v[i] + c[i-1][j-w[i]] > c[i-1][j]) {
                     // cout << "a\t";
                     c[i][j] = v[i] + c[i-1][j-w[i]];
                     p[i][j].assign( p[i-1][j-w[i]].begin(), p[i-1][j-w[i]].end());
                     p[i][j].push_back(i);
-                    for (int k = 0; k < p[i][j].size(); k++){
+                    for (int k = 0; k < p[i][j].size(); k++) {
                         // cout << p[i][j][k];
                     }
-                }
-                else{
+                } else {
                     // cout << "b\t";
                     c[i][j] = c[i-1][j];
                     p[i][j].assign( p[i-1][j].begin(), p[i-1][j].end());
-                    for (int k = 0; k < p[i][j].size(); k++){
+                    for (int k = 0; k < p[i][j].size(); k++) {
                         // cout << p[i][j][k];
                     }
                 }
-            }
-            else{
+            } else {
                 // cout << "c\t";
                 c[i][j] = c[i-1][j];
                 p[i][j].assign( p[i-1][j].begin(), p[i-1][j].end());
-                for (int k = 0; k < p[i][j].size(); k++){
+                for (int k = 0; k < p[i][j].size(); k++) {
                     // cout << p[i][j][k];
                 }
             }
@@ -152,10 +152,10 @@ void DP_0_1_knapsack(vector<int> &v, vector<int> &w, int n, int W){
     // Print answer
     cout << "Best value: " << c[n][W] <<  "\n";
     cout << "Best pattern:";
-    for (int i = 0; i < p[n][W].size(); i++){
+    for (int i = 0; i < p[n][W].size(); i++) {
         cout << " " << p[n][W][i];
     }
     cout << "\n";
-    
+
 
 }
